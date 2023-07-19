17575d6cb9216c8f90ec2fd897517e045868ed48
Tan <dinhtannguyen9x@gmail.com>
2023-07-19 11:06:39 +0700

    test

---
+++ src/pages/Loading/Loading.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import loadingAnimation from "./../../assets/animation/loading.json";
+import Lottie from "react-lottie";
+
+const Loading = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loadingAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div className="h-screen w-full flex items-center fixed  bg-white" style={{zIndex:"9999"}}>
+     {/* redux làm thêm 1 state, overFlowY : hidden */}
+      <Lottie options={defaultOptions} height={400} width={400} />
+    </div>
+  );
+};
+
+export default Loading;
